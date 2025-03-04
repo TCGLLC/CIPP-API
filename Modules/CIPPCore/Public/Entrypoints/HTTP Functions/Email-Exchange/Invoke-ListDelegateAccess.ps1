bf298f0b7e79b00ba059b066f892b41356b6b1fa
John J. Woloszyk <johnjwoloszyk@me.com>
2025-03-04 10:52:47 -0500

    Create Invoke-ListDelegateAccess.ps1

---
+++ Modules/CIPPCore/Public/Entrypoints/HTTP Functions/Email-Exchange/Invoke-ListDelegateAccess.ps1
@@ -0,0 +1,27 @@
+function Invoke-ListDelegateAccess {
+    <#
+    .FUNCTIONALITY
+        Entrypoint
+    .ROLE
+        Exchange.Mailbox.Read
+    #>
+    [CmdletBinding()]
+    param($Request, $TriggerMetadata)
+
+    $APIName = $TriggerMetadata.FunctionName
+    Write-LogMessage -user $request.headers.'x-ms-client-principal' -API $APINAME -message 'Accessed this API' -Sev 'Debug'
+
+    # Write to the Azure Functions log stream.
+    Write-Host 'PowerShell HTTP trigger function processed a request.'
+
+    # Interact with query parameters or the body of the request.
+    $TenantFilter = $Request.Query.TenantFilter
+
+    $Output = Get-CIPPEXODelegates -TenantFilter $TenantFilter -ExecutingUser $request.headers.'x-ms-client-principal'
+
+    # Associate values to output bindings by calling 'Push-OutputBinding'.
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::OK
+            Body       = $Output
+    })
+}
