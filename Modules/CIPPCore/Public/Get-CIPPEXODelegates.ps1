1be7b7c3e5dee04703f12c18c1387e158f309ea0
John J. Woloszyk <johnjwoloszyk@me.com>
2025-03-04 13:19:21 -0500

    Added cache support via datatables

--- Modules/CIPPCore/Public/Get-CIPPEXODelegates.ps1
+++ Modules/CIPPCore/Public/Get-CIPPEXODelegates.ps1
@@ -7,58 +7,145 @@ function Get-CIPPEXODelegates {
 	
 	$result = @()
 
-    try {    
-        Write-Host 'Fetching Mailboxes.'
-        $Mailboxes = New-ExoRequest -tenantid $TenantFilter -cmdlet 'Get-Mailbox' 
+	$Table = Get-CIPPTable -TableName CachedDelegateAccess
+	$Data = Get-CIPPAzDataTableEntity @Table -Filter "PartitionKey eq '$TenantFilter' and RowKey eq 'CachedResult'"
+    $currentTime = Get-Date -AsUTC
+    
+    if ($Data -eq $null) {
+        try {    
+            Write-Host 'Fetching Mailboxes.'
+            $Mailboxes = New-ExoRequest -tenantid $TenantFilter -cmdlet 'Get-Mailbox' 
 
-		foreach ($mb in $mailboxes) {
-            try {
-		        Write-Host "Processing  $($mb.UserPrincipalName)"
-			    $mailboxObj = [PSCustomObject]@{
-				    UPN                 = $mb.UserPrincipalName
-				    PrimarySmtpAddress  = $mb.PrimarySmtpAddress
-				    Permissions         = @()
-			    }
+		    foreach ($mb in $mailboxes) {
+                try {
+		            Write-Host "Processing  $($mb.UserPrincipalName)"
+			        $mailboxObj = [PSCustomObject]@{
+				        UPN                 = $mb.UserPrincipalName
+				        PrimarySmtpAddress  = $mb.PrimarySmtpAddress
+				        Permissions         = @()
+			        }
 			
-			    $fullAccessRaw = New-ExoRequest -tenantid $TenantFilter -cmdlet 'Get-MailboxPermission' -cmdParams @{Identity = $mb.Identity } -Anchor $mb.Identity
+			        $fullAccessRaw = New-ExoRequest -tenantid $TenantFilter -cmdlet 'Get-MailboxPermission' -cmdParams @{Identity = $mb.Identity } -Anchor $mb.Identity
 
-			    $fullAccess = $fullAccessRaw | Where-Object {
-				    $_.User -and $_.User.ToString() -notlike "NT AUTHORITY\SELF"
-			    }
-			    foreach ($perm in $fullAccess) {
-				    $mailboxObj.Permissions += [PSCustomObject]@{
-					    Delegate     = $perm.User.ToString()
-					    AccessRights = ($perm.AccessRights -join ", ")
-				    }
-			    }
+			        $fullAccess = $fullAccessRaw | Where-Object {
+				        $_.User -and $_.User.ToString() -notlike "NT AUTHORITY\SELF"
+			        }
+			        foreach ($perm in $fullAccess) {
+				        $mailboxObj.Permissions += [PSCustomObject]@{
+					        Delegate     = $perm.User.ToString()
+					        AccessRights = ($perm.AccessRights -join ", ")
+				        }
+			        }
 
-			    if ($mb.GrantSendOnBehalfTo) {
-				    foreach ($delegate in $mb.GrantSendOnBehalfTo) {
-					    $mailboxObj.Permissions += [PSCustomObject]@{
-						    Delegate     = $delegate.ToString()
-						    AccessRights = "SendOnBehalf"
-					    }
-				    }
-			    }
-                if($mailboxObj.Permissions.Count -eq 0) {
+			        if ($mb.GrantSendOnBehalfTo) {
+				        foreach ($delegate in $mb.GrantSendOnBehalfTo) {
+					        $mailboxObj.Permissions += [PSCustomObject]@{
+						        Delegate     = $delegate.ToString()
+						        AccessRights = "SendOnBehalf"
+					        }
+				        }
+			        }
+                    if($mailboxObj.Permissions.Count -eq 0) {
                     
-                } else { 
-                    $jsonPermissions = $mailboxObj.Permissions | ConvertTo-Json
-                    Write-Host "Finished processing $($mb.UserPrincipalName) as $jsonPermissions"
-			        $result += $mailboxObj
+                    } else { 
+                        $jsonPermissions = $mailboxObj.Permissions | ConvertTo-Json
+                        Write-Host "Finished processing $($mb.UserPrincipalName) as $jsonPermissions"
+			            $result += $mailboxObj
+                    }
+                } catch {
+                    Write-Host "Failed to process mailbox data: $($mb.UserPrincipalName)"
                 }
-            } catch {
-                Write-Host "Failed to process mailbox data: $($mb.UserPrincipalName)"
-            }
-		}
+		    }
            
-		# Convert the final result to JSON and output it
-		$returnResult = $result | ConvertTo-Json -Depth 5
-        Write-Host "Result: $returnResult"
-        return $returnResult
-    } catch {
-        $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
-        Write-Host "Result: $ErrorMessage"
-        return "Error in JW's custom function. "
+		    # Convert the final result to JSON and output it
+		    $returnResult = $result | ConvertTo-Json -Depth 5
+            Write-Host "Result: $returnResult"
+            $Data = @{
+				PartitionKey = "$TenantFilter"
+				RowKey       = "CachedResult"
+                FinishTimestamp = $currentTime
+				Data         = [string](ConvertTo-Json -InputObject $result -Depth 10 -Compress)
+			}
+            Add-CIPPAzDataTableEntity @Table -Entity $Data -Force
+            return $returnResult
+        } catch {
+            $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
+            Write-Host "Result: $ErrorMessage"
+            return "Error in JW's custom function. "
+        }
+    } else {
+        if($Data.FinishTimestamp) {
+            $finishTime = [datetime]$Data.FinishTimestamp
+            $timeDiff = $currentTime - $finishTime
+            if ($timeDiff.TotalHours -ge 2) {
+                Write-Verbose "Cache entry is older than 2 hours. Update needed."
+                $updateNeeded = $true
+            }
+       }
+       if($updateNeeded) {
+            try {    
+                    Write-Host 'Fetching Mailboxes.'
+                    $Mailboxes = New-ExoRequest -tenantid $TenantFilter -cmdlet 'Get-Mailbox' 
+
+		            foreach ($mb in $mailboxes) {
+                        try {
+		                    Write-Host "Processing  $($mb.UserPrincipalName)"
+			                $mailboxObj = [PSCustomObject]@{
+				                UPN                 = $mb.UserPrincipalName
+				                PrimarySmtpAddress  = $mb.PrimarySmtpAddress
+				                Permissions         = @()
+			                }
+			
+			                $fullAccessRaw = New-ExoRequest -tenantid $TenantFilter -cmdlet 'Get-MailboxPermission' -cmdParams @{Identity = $mb.Identity } -Anchor $mb.Identity
+
+			                $fullAccess = $fullAccessRaw | Where-Object {
+				                $_.User -and $_.User.ToString() -notlike "NT AUTHORITY\SELF"
+			                }
+			                foreach ($perm in $fullAccess) {
+				                $mailboxObj.Permissions += [PSCustomObject]@{
+					                Delegate     = $perm.User.ToString()
+					                AccessRights = ($perm.AccessRights -join ", ")
+				                }
+			                }
+
+			                if ($mb.GrantSendOnBehalfTo) {
+				                foreach ($delegate in $mb.GrantSendOnBehalfTo) {
+					                $mailboxObj.Permissions += [PSCustomObject]@{
+						                Delegate     = $delegate.ToString()
+						                AccessRights = "SendOnBehalf"
+					                }
+				                }
+			                }
+                            if($mailboxObj.Permissions.Count -eq 0) {
+                    
+                            } else { 
+                                $jsonPermissions = $mailboxObj.Permissions | ConvertTo-Json
+                                Write-Host "Finished processing $($mb.UserPrincipalName) as $jsonPermissions"
+			                    $result += $mailboxObj
+                            }
+                        } catch {
+                            Write-Host "Failed to process mailbox data: $($mb.UserPrincipalName)"
+                        }
+		            }
+           
+		            # Convert the final result to JSON and output it
+		            $returnResult = $result | ConvertTo-Json -Depth 5
+                    Write-Host "Result: $returnResult"
+                    $Data = @{
+				        PartitionKey = "$TenantFilter"
+				        RowKey       = "CachedResult"
+                        FinishTimestamp = $currentTime
+				        Data         = [string](ConvertTo-Json -InputObject $result -Depth 10 -Compress)
+			        }
+                    Add-CIPPAzDataTableEntity @Table -Entity $Data -Force
+                    return $returnResult
+                } catch {
+                    $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
+                    Write-Host "Result: $ErrorMessage"
+                    return "Error in JW's custom function. "
+                }
+       } else {
+            return $Data.Data
+       }
     }
 }
