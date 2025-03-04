47ec4ad782c684f482b634e6552af694023d8caa
John J. Woloszyk <johnjwoloszyk@me.com>
2025-03-04 10:30:44 -0500

    Create Get-CIPPEXODelegates.ps1

---
+++ Modules/CIPPCore/Public/Get-CIPPEXODelegates.ps1
@@ -0,0 +1,51 @@
+function Get-CIPPEXODelegates {
+    [CmdletBinding()]
+    param (
+        $TenantFilter,
+        $APIName = 'Get Delegate Permissions List',
+        $ExecutingUser
+    )
+	
+	$result = @()
+
+    try {
+        $Mailboxes = New-ExoRequest -tenantid $TenantFilter -cmdlet 'Get-Mailbox' 
+		
+		foreach ($mb in $mailboxes) {
+			$mailboxObj = [PSCustomObject]@{
+				UPN                 = $mb.UserPrincipalName
+				PrimarySmtpAddress  = $mb.PrimarySmtpAddress
+				Permissions         = @()
+			}
+			
+			$fullAccessRaw = New-ExoRequest -tenantid $TenantFilter -cmdlet 'Get-MailboxPermission' -cmdParams @{Identity = $mb.Identity } -Anchor $mb.Identity
+
+			$fullAccess = $fullAccessRaw | Where-Object {
+				$_.User -and $_.User.ToString() -notlike "NT AUTHORITY\SELF"
+			}
+			foreach ($perm in $fullAccess) {
+				$mailboxObj.Permissions += [PSCustomObject]@{
+					Delegate     = $perm.User.ToString()
+					AccessRights = ($perm.AccessRights -join ", ")
+				}
+			}
+
+			if ($mb.GrantSendOnBehalfTo) {
+				foreach ($delegate in $mb.GrantSendOnBehalfTo) {
+					$mailboxObj.Permissions += [PSCustomObject]@{
+						Delegate     = $delegate.ToString()
+						AccessRights = "SendOnBehalf"
+					}
+				}
+			}
+
+			$result += $mailboxObj
+		}
+
+		# Convert the final result to JSON and output it
+		return $result | ConvertTo-Json -Depth 5
+    } catch {
+        $ErrorMessage = Get-NormalizedError -Message $_.Exception.Message
+        return "Error in JW's custom function. "
+    }
+}
